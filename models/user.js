6075bb11358c4ceadbe5be84f4ee5721aa84f919
zerxal <44120703+hwgilbert16@users.noreply.github.com>
2021-07-18 10:15:43 -0400

    register.js email functionality

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const pool = require("../utils/db.js");
 const SqlString = require('sqlstring');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     // check if user exists in user table or unconfirmed users table
@@ -7,6 +8,7 @@ module.exports = {
         try {
             // escape characters
             const query = SqlString.format('SELECT email FROM users WHERE email=? union SELECT email FROM unconfirmed_users WHERE email=?', [email, email]);
+
             const conn = await pool.getConnection();
             const rows = await conn.query(query);
 
@@ -36,5 +38,36 @@ module.exports = {
         } catch (e) {
             throw e;
         }
+    },
+
+    // insert user into unconfirmed table
+    // have to do verification code in register.js since it doesn't make sense for this to return the code
+    async insertUnconfirmed(email, password, verificationCode) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            const query = SqlString.format('INSERT INTO unconfirmed_users (email, password, verification_code) VALUES (?, ?, ?)', [email, hashedPassword, verificationCode]);
+
+            const conn = await pool.getConnection();
+            const response = await conn.query(query);
+
+            conn.end();
+
+        } catch (e) {
+            throw e;
+        }
+    },
+
+    async verifyUser(verificationCode) {
+        const query = SqlString.format('INSERT INTO users (email, password) SELECT email, password FROM unconfirmed_users WHERE verification_code = ?', [verificationCode]);
+
+        const conn = await pool.getConnection();
+        const verifyUser = await conn.query(query);
+
+        console.log(verifyUser);
+
+        conn.end();
     }
+
 }
--- routes/register.js
+++ routes/register.js
@@ -1,20 +1,62 @@
 const express = require('express');
 const router = express.Router();
 const user = require('../models/user');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
 
 router.get('/', (req, res) => {
     res.render('register', {username: req.flash('username')});
+});
+
+router.get('/:verification_code', (req, res) => {
+    const verification_code = req.params.verification_code;
+    user.verifyUser(verification_code);
+
+    res.end();
 });
 
 router.post('/', (req, res) => {
     // get username and password into own consts
     const email = req.body.email;
     const password = req.body.password;
+    const verificationCode = crypto.randomBytes(16).toString('hex');
 
-    user.userExists(email)
-        .then((response) => console.log(response));
+    return Promise.all([
+        user.userExists(email),
+        user.insertUnconfirmed(email, password, verificationCode)
+    ]).then(() => {
+        /*
+        NEED TO CHECK HERE FOR PASSWORD LENGTH, IF EMAIL ALREADY EXISTS, ETC
+         */
 
-    res.end();
+        const messagebody = `
+        Hello,
+        
+        Please click the link below to verify your account.
+        
+        http://localhost:3000/verify/${verificationCode}
+        `;
+
+        const transporter = nodemailer.createTransport('smtp://hwgilbert16@gmail.com:tjzecesmgkxgpmsw@smtp.gmail.com');
+        const mailOptions = {
+            from: 'hwgilbert16@gmail.com',
+            to: 'hwgilbert16@gmail.com',
+            subject: 'Email Verification',
+            text: messagebody
+        };
+
+        transporter.sendMail(mailOptions, (err, data) => {
+            if (err) {
+                console.log('Error');
+            } else {
+                console.log('Email sent successfully');
+            }
+        });
+
+        res.redirect('/hello');
+    }).catch((e) => {
+        throw e;
+    })
 });
 
 module.exports = router;
