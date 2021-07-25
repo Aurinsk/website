2600c011095b871a78350fa6c6eb5085a3e8277a
zerxal <44120703+hwgilbert16@users.noreply.github.com>
2021-07-24 21:59:10 -0400

    add primitive monitor listing

--- public/js/minecraftmonitors-front.js
+++ public/js/minecraftmonitors-front.js
@@ -1,97 +1,130 @@
-$(document).ready(() => {
+$.get('http://192.168.1.251/api/query/hwgilbert16@gmail.com', ((data) => {
+    for (const row of data) {
+        const monitor = document.createElement('tr');
+        const name = document.createElement('td');
+        name.textContent = row.name;
+        monitor.append(name);
+        $('tbody').append(monitor);
+    }
+}));
 
-    $('#addMonitorForm').submit((e) => {
-        e.preventDefault();
+$('#addMonitorForm').submit((e) => {
+    e.preventDefault();
 
-        const monitorName = $('#minecraftMonitorName').val();
-        $('#addMonitorFormSubmit').attr('hidden', 'true');
-        $('#ipAddress').attr('disabled', 'true');
-        $('#minecraftMonitorName').attr('disabled', 'true');
+    const monitorName = $('#minecraftMonitorName').val();
+    $('#addMonitorFormSubmit').attr('hidden', 'true');
+    $('#ipAddress').attr('disabled', 'true');
+    $('#minecraftMonitorName').attr('disabled', 'true');
 
-        const downloadMessage = document.createElement('p');
-        downloadMessage.textContent = 'Download and install the monitoring plugin from here, and install it into your plugins folder. Once installed, reboot your server. We will detect once we have received analytics below.';
-        downloadMessage.id = "downloadMessage";
+    $('.alert-secondary').remove();
+    $('#receivedAnalytics').remove();
+    $('#alreadyExists').remove();
+    $('#downloadMessage').remove();
 
-        const analyticsChecker = document.createElement('div');
-        analyticsChecker.className = "alert alert-secondary text-center";
-        const analyticsCheckerSpinner = document.createElement('span');
-        analyticsCheckerSpinner.className = 'spinner-border';
-        const analyticsCheckerMessage = document.createElement('p');
-        analyticsCheckerMessage.textContent = 'Waiting for analytics...';
+    const downloadMessage = document.createElement('p');
+    downloadMessage.textContent = 'Download and install the monitoring plugin from here, and install it into your plugins folder. Once installed, reboot your server. We will detect once we have received analytics below.';
+    downloadMessage.id = "downloadMessage";
 
-        $('.mb-3.ip-address').append(downloadMessage, analyticsChecker);
-        $('.alert-secondary').append(analyticsCheckerSpinner, analyticsCheckerMessage);
+    const analyticsChecker = document.createElement('div');
+    analyticsChecker.className = "alert alert-secondary text-center";
+    analyticsChecker.id = 'analyticsChecker';
+    const analyticsCheckerSpinner = document.createElement('span');
+    analyticsCheckerSpinner.className = 'spinner-border';
+    const analyticsCheckerMessage = document.createElement('p');
+    analyticsCheckerMessage.textContent = 'Waiting for analytics...';
 
-        function getCookie(name) {
-            let cookieArr = document.cookie.split(";");
+    $('.mb-3.ip-address').append(downloadMessage, analyticsChecker);
+    $('.alert-secondary').append(analyticsCheckerSpinner, analyticsCheckerMessage);
 
-            for (let i = 0; i < cookieArr.length; i++) {
-                let cookiePair = cookieArr[i].split("=");
+    function getCookie(name) {
+        let cookieArr = document.cookie.split(";");
 
-                if(name == cookiePair[0].trim()) {
-                    return decodeURIComponent(cookiePair[1]);
-                }
+        for (let i = 0; i < cookieArr.length; i++) {
+            let cookiePair = cookieArr[i].split("=");
+
+            if(name == cookiePair[0].trim()) {
+                return decodeURIComponent(cookiePair[1]);
+            }
+        }
+
+        return null;
+    }
+
+    const interval = setInterval(() => {
+        $.post('http://192.168.1.251/api/create', {ip: $('#ipAddress').val(), email: getCookie('email'), name: $('#minecraftMonitorName').val()}, (data) => {
+            if (data === 'exists') {
+                const alreadyExists = document.createElement('div');
+                alreadyExists.className = 'alert alert-danger';
+                alreadyExists.id = 'alreadyExists';
+                alreadyExists.textContent = 'The monitor belonging to this IP address is already owned by you or somebody else';
+
+                $('.mb-3.ip-address').append(alreadyExists);
+                $('#analyticsChecker').remove();
+
+                $('#minecraftMonitorName').removeAttr('disabled');
+                $('#ipAddress').removeAttr('disabled');
+                $('#addMonitorFormSubmit').removeAttr('hidden');
+
+                clearInterval(interval);
             }
 
-            return null;
-        }
+            if (data === 'true') {
+                $('.alert-secondary').remove();
 
-        const interval = setInterval(() => {
-            $.post('http://192.168.1.251/api/create', {ip: $('#ipAddress').val(), email: getCookie('email'), name: $('#minecraftMonitorName').val()}, (data) => {
-                if (data === 'true') {
-                    $('.alert-secondary').remove();
+                const receivedAnalytics = document.createElement('div');
+                receivedAnalytics.className = 'alert alert-success text-center';
+                receivedAnalytics.id = 'receivedAnalytics';
+                const analyticsReceivedCheck = document.createElement('span');
+                analyticsReceivedCheck.className = 'bi bi-check-lg';
 
-                    const receivedAnalytics = document.createElement('div');
-                    receivedAnalytics.className = 'alert alert-success text-center';
-                    const analyticsReceivedCheck = document.createElement('span');
-                    analyticsReceivedCheck.className = 'bi bi-check-lg';
-                    $('.mb-3.ip-address').append(receivedAnalytics);
-                    $('.alert-success').append(analyticsReceivedCheck);
+                const viewMonitor = document.createElement('button');
+                viewMonitor.className = 'btn btn-primary';
+                viewMonitor.textContent = 'View your Monitors';
+                viewMonitor.setAttribute('data-bs-dismiss', 'modal');
 
-                    clearInterval(interval);
-                }
-            })
-        }, 2500);
+                $('.mb-3.ip-address').append(receivedAnalytics, viewMonitor);
+                $('.alert-success').append(analyticsReceivedCheck);
 
-    });
+                clearInterval(interval);
+            }
+        })
+    }, 2500);
 
-    $('#addMonitorModal').on('show.bs.modal', () => {
-        window.onbeforeunload = () => {
-            return true;
-        }
-    });
-
-    $('#addMonitorModal').on('hidden.bs.modal', () => {
-        $('#minecraftMonitorName').val('');
-        $('#ipAddress').val('');
-        $('#addMonitorFormSubmit').removeAttr('hidden');
-        $('#minecraftMonitorName').removeAttr('disabled');
-        $('#ipAddress').removeAttr('disabled');
-        $('#downloadMessage').remove();
-
-        if ($('.alert-secondary').length) {
-            $('.alert-secondary').remove();
-        }
-
-        if ($('.alert-success').length) {
-            $('.alert-success').remove();
-        }
-
-        if (typeof interval !== 'undefined') {
-            clearInterval(interval);
-        }
-
-        window.onbeforeunload = null;
-    });
 });
 
-function receiveAnalytics() {
-    $('.alert-secondary').remove();
+$('#addMonitorModal').on('show.bs.modal', () => {
+    window.onbeforeunload = () => {
+        return true;
+    }
+});
 
-    const receivedAnalytics = document.createElement('div');
-    receivedAnalytics.className = 'alert alert-success text-center';
-    const analyticsReceivedCheck = document.createElement('span');
-    analyticsReceivedCheck.className = 'bi bi-check-lg';
-    $('.mb-3').append(receivedAnalytics);
-    $('.alert-success').append(analyticsReceivedCheck);
-}
+$('#addMonitorModal').on('hidden.bs.modal', () => {
+    $('#minecraftMonitorName').val('');
+    $('#ipAddress').val('');
+    $('#addMonitorFormSubmit').removeAttr('hidden');
+    $('#minecraftMonitorName').removeAttr('disabled');
+    $('#ipAddress').removeAttr('disabled');
+    $('#downloadMessage').remove();
+
+    $('.alert-secondary').remove();
+    $('#receivedAnalytics').remove();
+    $('#alreadyExists').remove();
+
+    // if ($('#analyticsChecker').length) {
+    //     $('.alert-secondary').remove();
+    // }
+    //
+    // if ($('#receivedAnalytics').length) {
+    //     $('.alert-success').remove();
+    // }
+    //
+    // if ($('#alreadyExists').length) {
+    //     $('#alreadyExists').remove();
+    // }
+
+    if (typeof interval !== 'undefined') {
+        clearInterval(interval);
+    }
+
+    window.onbeforeunload = null;
+});
