a6f3dbbb57eb60a1834b8895cd1e6048ecfe7b80
zerxal <44120703+hwgilbert16@users.noreply.github.com>
2021-08-06 13:53:43 -0400

    jwt error handling and clear email cookie on logout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   const userCookie = req.cookies.user;
-  console.log(userCookie);
   if (req.flash('invalid').length > 0) {
     res.render('index', {invalid: true, user: userCookie});
   } else {
--- routes/logout.js
+++ routes/logout.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.clearCookie('user');
+    res.clearCookie('email');
     res.redirect('/');
 })
 
